ddba5189dbdd1f7ada70d4515b3c2e69d1f7ca97
WooSunghyeon <85105077+WooSunghyeon@users.noreply.github.com>
2021-06-05 00:27:58 +0900

    Update basic_blocks.py

--- code/basic_blocks.py
+++ code/basic_blocks.py
@@ -7,18 +7,18 @@ from building_blocks import *
 class Dfa_Block(nn.Module): # for DFA in ResNet
     def __init__(self, connect_channels):
         super(Dfa_Block, self).__init__()
-        self.dfa = Feedback_Reciever(connect_channels)
+        self.dfa = Feedback_Receiver(connect_channels)
     
     def forward(self, x):
         x, dm1 = self.dfa(x)
         self.dummy = dm1
         return x
     
-class LW_Conv_Block(nn.Module): # for ASAP in ConvNet
+class ASAP_Conv_Block(nn.Module): # for ASAP in ConvNet
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, wt = False):
-        super(LW_Conv_Block, self).__init__()
+        super(ASAP_Conv_Block, self).__init__()
         
-        self.conv = Conv2d_FA_LW(in_channels, out_channels, kernel_size, stride, padding, wt = wt)
+        self.conv = Conv2d_FA_ASAP(in_channels, out_channels, kernel_size, stride, padding, wt = wt)
         self.bn = nn.BatchNorm2d(out_channels)
         self.max = nn.MaxPool2d(2,2, ceil_mode = True)
         
